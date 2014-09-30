dfc0ef9648a634845965553086e9567f27c1c6e2
kmb32123 <breyk@uwplatt.edu>
2014-09-30 00:52:57 -0500

    Created Movie Page
    Added initial movie page that lists movies in a table and filtering

--- build.config.js
+++ build.config.js
@@ -66,9 +66,13 @@ module.exports = {
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/angular-ui-utils/modules/route/route.js',
+      'vendor/angular-ui-select/dist/select.min.js',
+      'vendor/angular-ui-utils/ui-utils.min.js',
+      'vendor/angular-sanitize/angular-sanitize.min.js'
     ],
     css: [
+        'vendor/angular-ui-select/dist/select.min.css'
     ],
     assets: [
     ]
--- src/app/movies/movies.js
+++ src/app/movies/movies.js
@@ -1,7 +1,10 @@
 angular.module( 'ngBoilerplate.movies', [
   'ui.router',
   'placeholders',
-  'ui.bootstrap'
+  'ui.select',
+  'ui.unique',
+  'ui.bootstrap',
+  'ngSanitize'
 ])
 
 .config(function config( $stateProvider ) {
@@ -31,13 +34,126 @@ angular.module( 'ngBoilerplate.movies', [
   });
 })
 
-.controller( 'MoviesCtrl', function MoviesCtrl( $scope ) {
-  // This is simple a demo for UI Boostrap.
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
+.factory('movieRetrievalService', function ($http, $q) {
+
+    function Request () {
+        this.request = {};
+        this.request.jsonrpc = "2.0";
+        this.request.method = "VideoLibrary.GetMovies";
+        this.request.params = {};
+        this.request.params.limits = {};
+        this.request.params.limits.start = 0;
+        this.request.params.limits.end = 200;
+        this.request.params.properties = ["title","genre","year","mpaa","rating"];
+        this.request.id = "libMovies";
+        
+        this.encode = function() {
+            return encodeURIComponent(JSON.stringify(this.request));
+        };
+    }
+
+    var deffered = $q.defer();
+    var data = [];
+    var movieRetrievalService = {};
+
+    var request = new Request();
+    var url = 'http://localhost/jsonrpc?request=' + request.encode();
+    
+    console.log(request);
+    movieRetrievalService.async = function () {
+        $http.get(url)
+            .success(function (d) {
+                data = d;
+                console.log(d);
+                deffered.resolve();
+            });
+        return deffered.promise;
+    };
+    movieRetrievalService.data = function () {
+        return data;
+    };
+
+    return movieRetrievalService;
+})
+
+.controller( 'MoviesCtrl', function MoviesCtrl( movieRetrievalService, $scope, $filter ) {
+    $scope.isCollapsed = true;
+
+    $scope.columns = [{
+        property: function (i) {
+            return i.label;
+        },
+        name: "label",
+        header: "Title"
+    }, {
+        property: function (i) {
+            return i.year;
+        },
+        name: "year",
+        header: "Year"
+    }, {
+        property: function (i) {
+            return i.mpaa;
+        },
+        name: "mpaa",
+        header: "MPAA Rating"
+    }, {
+        property:  function (i) {
+            return $filter('number')(i.rating, 1);
+        },
+        name: "rating",
+        header: "Rating"
+    }];
+
+    $scope.setSorting = function (columnName) {
+        var sort = $scope.sort;
+        if (sort.columnName == columnName) {
+            sort.descending = !sort.descending;
+            if ($scope.sort.descending) {
+                sort.sortTerm = "-" + $scope.sort.columnName;
+            } else {
+                sort.sortTerm = columnName;
+            }
+        } else {
+            sort.sortTerm = sort.columnName = columnName;
+            sort.descending = false;
+        }
+    };
+
+    $scope.clear = function() {
+        $scope.genre.selected = undefined;
+    };
+
+    $scope.genre = {};
+    $scope.genres = [];
+
+
+    $scope.rating = {};
+    $scope.ratings = [];
+
+    $scope.sort = {
+        columnName: '',
+        descending: false,
+        sortTerm: ''
+    };
+
+    movieRetrievalService.async().then(function () {
+        $scope.data = movieRetrievalService.data();
+        
+        var genres = [];
+        $scope.data.result.movies.forEach(function(entry) {
+            genres = genres.concat(entry.genre);
+        });
+        
+        $scope.genres = genres;
+        
+        var ratings = [];
+        $scope.data.result.movies.forEach(function(entry) {
+            ratings.push(entry.mpaa);
+        });
+        
+        $scope.ratings = ratings;
+    });
 })
 
 ;
