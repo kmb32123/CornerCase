1cd6a83447c47e98bc05b39fda76f37be59c9223
kmb32123 <breyk@uwplatt.edu>
2014-10-09 00:53:55 -0500

    Added song list display on music tab

--- src/app/music/music.js
+++ src/app/music/music.js
@@ -41,13 +41,116 @@ angular.module( 'ngBoilerplate.music', [
   });
 })
 
-.controller( 'MusicCtrl', function MusicCtrl( $scope ) {
-  // This is simple a demo for UI Boostrap.
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
+.factory('songRetrievalService', function ($http, $q) {
+    
+    function Request () {
+        this.request = {};
+        this.request.jsonrpc = "2.0";
+        this.request.method = "AudioLibrary.GetSongs";
+        this.request.params = {};
+        this.request.params.limits = {};
+        this.request.params.limits.start = 0;
+        this.request.params.limits.end = 200;
+        this.request.params.properties = ["artist","duration","album","track"];
+        this.request.id = "libSongs";
+        
+        this.encode = function() {
+            return encodeURIComponent(JSON.stringify(this.request));
+        };
+    }
+
+    var deffered = $q.defer();
+    var data = [];
+    var songRetrievalService = {};
+    
+    var request = new Request();
+    var url = 'http://localhost/jsonrpc?request=' + request.encode();
+    
+    console.log(request);
+    songRetrievalService.async = function () {
+        $http.get(url)
+            .success(function (d) {
+                data = d;
+                console.log(d);
+                deffered.resolve();
+            });
+        return deffered.promise;
+    };
+    songRetrievalService.data = function () {
+        return data;
+    };
+
+    return songRetrievalService;
+})
+
+.controller( 'MusicCtrl', function MusicCtrl( songRetrievalService, $scope ) {
+    $scope.isCollapsed = true;
+
+    $scope.columns = [{
+        property: function (i) {
+            return i.songid;
+        },
+        name: "songid",
+        header: "ID"
+    }, {
+        property: function (i) {
+            return i.track;
+        },
+        name: "track",
+        header: "Track"
+    }, {
+        property: function (i) {
+            return i.label;
+        },
+        name: "label",
+        header: "Title"
+    }, {
+        property: function (i) {
+            return i.album;
+        },
+        name: "album",
+        header: "Album"
+    }, {
+        property: function (i) {
+            return i.artist.join(",");
+        },
+        name: "artist",
+        header: "Artist"
+    }, {
+        property: function (i) {
+            seconds = i.duration;
+            minutes = (i.duration / 60) | 0;
+            seconds -= (minutes * 60);
+            return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+        },
+        name: "duration",
+        header: "Length"
+    }];
+
+    $scope.setSorting = function (columnName) {
+        var sort = $scope.sort;
+        if (sort.columnName == columnName) {
+            sort.descending = !sort.descending;
+            if ($scope.sort.descending) {
+                sort.sortTerm = "-" + $scope.sort.columnName;
+            } else {
+                sort.sortTerm = columnName;
+            }
+        } else {
+            sort.sortTerm = sort.columnName = columnName;
+            sort.descending = false;
+        }
+    };
+
+    $scope.sort = {
+        columnName: '',
+        descending: false,
+        sortTerm: ''
+    };
+
+    songRetrievalService.async().then(function () {
+        $scope.data = songRetrievalService.data();
+    });
 })
 
 ;
